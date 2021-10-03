a4b3130fae87f17fc0dfa43395285f99c9f1b94d
SHolikov <murodil@gmail.com>
2021-10-03 11:47:36 -0500

    updated third file

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -4,4 +4,12 @@ public class Third {
     public static void main(String[] args) {
         System.out.println(" We are learning Git");
     }
+
+    public void add(int a){
+
+
+    }
+    public void add( int a, int b){
+
+    }
 }
